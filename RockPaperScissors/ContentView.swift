26fc433360813a5f7341511120d94a553d71708a
Oliwier Kaspprzak <oliwier.kasprzak00gmail.com>
2023-04-14 14:09:41 +0200

    All functionality working.

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,96 @@
 import SwiftUI
 
 struct ContentView: View {
+    let moves = ["✊", "✋", "✌️"]
+    @State private var computerChoice = Int.random(in: 0..<3)
+    @State private var shouldWin = Bool.random()
+    
+    @State private var score = 0
+    @State private var showingResults = false
+    @State private var questionCount = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background1")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                
+                Text("Computer's choice")
+                    .font(.title2.bold())
+                    .foregroundColor(.cyan)
+                
+                Text(moves[computerChoice])
+                    .font(.system(size: 120))
+                if shouldWin {
+                    Text("Which one wins?")
+                        .font(.headline)
+                        .foregroundColor(.green)
+                } else {
+                    Text("Which one loses?")
+                        .font(.headline)
+                        .foregroundColor(.red)
+                }
+                
+                HStack {
+                    ForEach(0..<3) { number in
+                        Button {
+                            play(choice: number)
+                        } label: {
+                            Text(moves[number])
+                                .font(.system(size: 80))
+                        }
+                    }
+                }
+                Spacer()
+                Text("Score: \(score)")
+                    .padding(5)
+                    .foregroundColor(.cyan)
+                    .font(.title2.bold())
+                    .background(Color.white.opacity(0.5))
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                    
+                Spacer()
+            }
+            .alert("The game is over!", isPresented: $showingResults) {
+                Button("Play again?", action: restart)
+            } message: {
+                Text("Final score is \(score) ")
+            }
         }
-        .padding()
+    }
+    
+    func play(choice: Int) {
+        let winningMoves = [1, 2, 0]
+        var didWin: Bool
+        
+        if shouldWin {
+            didWin = choice == winningMoves[computerChoice]
+        } else {
+            didWin = winningMoves[choice] == computerChoice
+        }
+        
+        if didWin {
+            score += 1
+        } else {
+            score -= 1
+        }
+        
+        if questionCount == 8 {
+            showingResults = true
+        } else {
+            computerChoice = Int.random(in: 0..<3)
+            questionCount += 1
+            shouldWin.toggle()
+        }
+    }
+    
+    func restart() {
+        score = 0
+        questionCount = 1
+        computerChoice = Int.random(in: 0..<3)
+        shouldWin.toggle()
     }
 }
 
